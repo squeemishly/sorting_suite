a2e1c077b234dc460dc5539120e606e4865b2140
squeemishly <squeemishly@gmail.com>
2017-03-18 11:48:02 -0600

    cleaned up a little

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,27 +6,19 @@
 #5. repeat for the whole collection until everything is sorted
 #6. rejoice in the .sort method from this day forward.
 
-numbers = ["d", "b", "a", "c"]
-
 class BubbleSort
-    def initialize (to_be_sorted)
-        @to_be_sorted = to_be_sorted
-    end
 
-    def sort
+    def sort (to_be_sorted)
         count = 0
         a = 0
         while count < to_be_sorted.length
-            to_be_sorted.each_with_index do |number, index|
+            to_be_sorted.each_with_index do |value, index|
                 if index == to_be_sorted.length - 1
                     next
-                elsif number > to_be_sorted[index+1]
-                    a = number
+                elsif value > to_be_sorted[index+1]
+                    a = value
                     to_be_sorted.delete(a)
                     to_be_sorted.insert(index+1, a)
-                    p to_be_sorted
-                else
-                    puts "yay"
                 end
             end
         count += 1
@@ -36,5 +28,4 @@ class BubbleSort
 end
 
 sorter = BubbleSort.new
-sorter.sort(numbers)
-puts "Now I shall rejoice in .sort for all time."
+sorter.sort(["d", "b", "a", "c"])

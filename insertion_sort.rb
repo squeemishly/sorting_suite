34fe49f2fd2391f39a75ac048f39b6c37cb93ce0
squeemishly <squeemishly@gmail.com>
2017-03-20 08:35:14 -0600

    still failing

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,6 +1,10 @@
 unsorted_list = [1,0,4,3,2]
 sorted_list = []
 
+def add_next_value(list)
+  
+end
+
 while unsorted_list.length > 0
   stored_value = unsorted_list.delete(unsorted_list[0])
   if sorted_list.length == 0
@@ -13,7 +17,7 @@ while unsorted_list.length > 0
         if stored_value < sorted_list[index]
           sorted_list.unshift(stored_value)
         else
-          sorted_list << stored_value
+          sorted_list.insert(index,stored_value)
         end
       end
     end
